07dc762780e454d1de6a9c1c13084f002ada1c4e
echavez85 <echavez85@gmail.com>
2018-11-12 22:47:12 -0600

    Started js logic. Added firebase var.

---
+++ javascript/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCCozKRv4wWm7K60tmjbIcGgj3nlqtKC3c",
+    authDomain: "train-scheduler-e91a2.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e91a2.firebaseio.com",
+    projectId: "train-scheduler-e91a2",
+    storageBucket: "train-scheduler-e91a2.appspot.com",
+    messagingSenderId: "285721104765"
+  };
+  firebase.initializeApp(config);
