cdefe062d4a4b496f5b4ba6a03b560bb91fd81a8
echavez85 <echavez85@gmail.com>
2018-11-12 22:57:51 -0600

    Pseudocoded logic.js

--- javascript/logic.js
+++ javascript/logic.js
@@ -8,3 +8,22 @@ var config = {
     messagingSenderId: "285721104765"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+// Add button for adding trains
+    //   Grabs user input
+    //   Create local "temporary" object for holding train data
+    //   Upload train data to the database
+    //   Log everything to console
+    //   Clear all of the input boxes
+    
+    
+// Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+    // Store everything into a variable
+    // Console log the train info
+    // Chance train time to unix w/ moment
+    // Calculate minutes until arrival
+    // Create the new row
+    // Append the new row to the table
+    
